f4628028a24064b8860ec2c5a0c1d2568f4f309c
Ekaterina Mozhegova <mozhegova04@gmail.com>
2023-07-12 23:40:44 +0300

    Created a module for splitting an audio and retrieving a transcription from it. Works for three langs.

--- modules/audio_modules/AudioFileWithTextLangs.py
+++ modules/audio_modules/AudioFileWithTextLangs.py
@@ -35,7 +35,7 @@ class AudioFileWithTextLangs:
         # Iterate over all segments, collecting all words in transcription
         for sentence in response_dic:
             for word in sentence['words']:
-                transcription.append(word)
+                transcription.append(word['word'])
 
         return transcription
 
