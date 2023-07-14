32d61f0461f71f7df8cb4768268f5fbebbe4335b
Ilya Krasheninnikov <il.krasheninnikov@innopolis.university>
2023-07-14 15:27:03 +0300

    Updated recognition modules and endpoint audio_split_by_image

--- modules/audio_modules/AudioFileWithText.py
+++ modules/audio_modules/AudioFileWithText.py
@@ -1,58 +1,79 @@
-from pydub import AudioSegment
 import whisper
+from pydub import AudioSegment
+
 
 class AudioFileWithText:
     def __init__(self, audio_file_path: str):
+        # Initialize class with audio file path
         self.audio_file_path = audio_file_path
 
-    def get_timed_recognised_text(self):
-        # Load the whisper model
-        model = whisper.load_model('small')
-        result = model.transcribe(self.audio_file_path, fp16=False, word_timestamps=True)
-        result = result["segments"][0]['words']
+    @staticmethod
+    # Function to flatten a list of lists
+    def flat_map(fn, list_):
+        result = []
+        # Apply function to every item in list, extending the result list
+        for item in list_:
+            result.extend(fn(item))
         return result
 
+    def get_timed_recognised_text(self):
+        """
+        Transcribes audio into text
+        :parameter audio_name - name of input audio
+        :return transcription - the transcribed text
+        """
+        # Load the whisper model
+        model = whisper.load_model('small')
+        # Transcribe the audio file into words with timestamps. 16-bit floating point precision
+        response = model.transcribe(self.audio_file_path, fp16=False, word_timestamps=True)
+        response_dic = response["segments"]
+
+        transcription = []
+        # Iterate over all segments, collecting all words in transcription
+        for sentence in response_dic:
+            for word in sentence['words']:
+                transcription.append(word)
+
+        return transcription
+
     def get_end_time_of_each_sentence(self, last_words):
+        # Obtain timed transcriptions
         timed_recognised_text = self.get_timed_recognised_text()
-        mapping = list(
-            map(lambda last_word:
-                list(
-                    map(lambda filtered_transcription: (
-                        filtered_transcription['end']),
-                        filter(lambda transcription:
-                               last_word in transcription['word'], timed_recognised_text))),
-                last_words))
-        final_timestamps = [0]
-        final_timestamps += flat_map(lambda lst: lst, mapping)
-        return final_timestamps
+        mapping = []
+
+        # For each 'last word', find matching transcriptions and store their end times
+        for last_word in last_words:
+            matching_transcriptions = []
+            for transcription in timed_recognised_text:
+                if last_word in transcription['word']:
+                    matching_transcriptions.append(transcription['end'])
+            mapping.append(matching_transcriptions)
+
+        # Create list of end times for all words in mapping
+        word_end_timestamps = [0]
+        word_end_timestamps += self.flat_map(lambda lst: lst, mapping)
+        return word_end_timestamps
 
     def split_audio_by_last_words(self, last_words):
+        """
+        Splits the audio file into segments based on the 'last words' of sentences.
+        """
+        # Load audio file
         audio = AudioSegment.from_file(self.audio_file_path, format="mp3")
 
         segments = []
+        # Obtain timestamps for all sentences' end times
         timestamps = self.get_end_time_of_each_sentence(last_words)
-        for i, timestamp in enumerate(timestamps):
+        # Iterate over all timestamps, splitting audio into segments
+        for timestamp_index, timestamp in enumerate(timestamps):
             start_time = int(timestamp * 1000)
-            if i < len(timestamps) - 1:
-                end_time = int(timestamps[i + 1] * 1000)
+            if timestamp_index < len(timestamps) - 1:
+                end_time = int(timestamps[timestamp_index + 1] * 1000)
             else:
-                end_time = len(audio)  # Set the last timestamp as the end of the audio
+                # For the last timestamp, use the end of the audio
+                end_time = len(audio)
 
+            # Create the audio segment
             segment = audio[start_time:end_time]
             segments.append(segment)
         return segments
-
-
-def audio_transcribe(audio_path):
-    # Load the whisper model
-    model = whisper.load_model('small')
-    result = model.transcribe(audio_path, fp16=False, word_timestamps=True)
-    result = result["segments"][0]['words']
-    return result
-
-# Function to flatten a list of lists
-def flat_map(f, xs):
-    ys = []
-    for x in xs:
-        ys.extend(f(x))
-    return ys
