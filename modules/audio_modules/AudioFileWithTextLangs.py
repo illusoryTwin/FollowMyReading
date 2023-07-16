e42d74085e972d7ec5cdd2eb10b2c3ec3e0cffae
Ekaterina Mozhegova <mozhegova04@gmail.com>
2023-07-16 19:09:17 +0300

    Updated a module

--- modules/audio_modules/AudioFileWithTextLangs.py
+++ modules/audio_modules/AudioFileWithTextLangs.py
@@ -5,6 +5,8 @@
 import whisper
 from pydub import AudioSegment
 
+
+
 class AudioFileWithTextLangs:
     def __init__(self, audio_file_path: str):
         # Initialize class with audio file path
@@ -39,6 +41,26 @@ class AudioFileWithTextLangs:
 
         return transcription
 
+    def get_words_from_audio(self, lang_code):
+            """
+            Transcribes audio into text
+            :parameter audio_name - name of input audio
+            :return words - a list of transcribed words
+            """
+            # Load the whisper model
+            model = whisper.load_model('small')
+            # Transcribe the audio file into words with timestamps. 16-bit floating point precision
+            response = model.transcribe(self.audio_file_path, fp16=False, word_timestamps=True, language=lang_code)
+            response_dic = response["segments"]
+
+            words = []
+            # Iterate over all segments, collecting all words in transcription
+            for sentence in response_dic:
+                for word in sentence['words']:
+                    words.append(word['word'])
+
+            return words
+
 
     def get_end_time_of_each_sentence(self, last_words, lang_code: str):
         # Obtain timed transcriptions
@@ -49,6 +71,8 @@ class AudioFileWithTextLangs:
         for last_word in last_words:
             matching_transcriptions = []
             for transcription in timed_recognised_text:
+                #print(last_word)
+                #print("transcription", transcription)
                 if last_word in transcription['word']:
                     matching_transcriptions.append(transcription['end'])
             mapping.append(matching_transcriptions)
@@ -70,7 +94,7 @@ class AudioFileWithTextLangs:
         timestamps = self.get_end_time_of_each_sentence(last_words, lang_code)
         # No need to split by the very last word
         timestamps = timestamps[:-1]
-        print("timestamps", timestamps)
+        # print("timestamps", timestamps)
         # Iterate over all timestamps, splitting audio into segments
         for timestamp_index, timestamp in enumerate(timestamps):
             start_time = int(timestamp * 1000)
