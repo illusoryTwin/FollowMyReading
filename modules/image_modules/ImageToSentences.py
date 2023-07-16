a92f168f539cceb2048fa24b96a633825209accf
Ekaterina Mozhegova <mozhegova04@gmail.com>
2023-07-16 20:49:01 +0300

    Updated an image module

--- modules/image_modules/ImageToSentences.py
+++ modules/image_modules/ImageToSentences.py
@@ -1,17 +1,10 @@
-<<<<<<< Updated upstream:modules/image_modules/ImageToSentences.py
-# !apt install tesseract-ocr
-# !pip install pytesseract
-
-=======
 import re
->>>>>>> Stashed changes:app/modules/image_modules/ImageToSentences.py
 import pytesseract
 from PIL import Image
 
-#pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+# pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 
-
-class ImageToSentences:
+class ImageWithText:
     """Class for handling an image with text."""
 
     def __init__(self, image: Image):
@@ -27,39 +20,6 @@ class ImageToSentences:
             print("Unsupported language!")
             return None
 
-<<<<<<< Updated upstream:modules/image_modules/ImageToSentences.py
-    @staticmethod
-    def find_word_before_punct_mark(text):
-        """
-        Function to find the last word before each punctuation mark in the text.
-        """
-        words_array = text.split(" ")
-        last_words_array = []
-        sentence_end_marks = ['.', ',', '?', '!']
-
-        for word_index in range(len(words_array)):
-            if words_array[word_index] in sentence_end_marks:
-                if word_index > 0:
-                    word_before_dot = words_array[word_index - 1]
-                    last_words_array.append(word_before_dot)
-
-        return last_words_array
-
-    def get_sentences(self):
-        """
-        Extract text from the image.
-        Returns a string that represents the extracted text from the image.
-        """
-        # Extract text from the image
-        image_text = pytesseract.image_to_string(self.image)
-        return image_text
-
-# # Example of usage
-# image = Image.open("часть 1.png")
-# image_with_text = ImageWithText(image)
-# sentences = image_with_text.get_sentences()
-# print(sentences)
-=======
         # Extract text from the image
         image_text = pytesseract.image_to_string(self.image, lang=lang_code)
 
@@ -90,4 +50,3 @@ class ImageToSentences:
 #
 # last_words = image.get_sentences(lang_code)
 # print(last_words)
->>>>>>> Stashed changes:app/modules/image_modules/ImageToSentences.py
